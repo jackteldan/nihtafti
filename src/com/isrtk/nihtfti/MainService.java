24c7ec9482f8ffb0b209f82a2f2f0aae8f503373
Avi <siter4u@gmail.com>
2014-06-18 06:45:56 +0300

    avi

---
+++ src/com/isrtk/nihtfti/MainService.java
@@ -0,0 +1,15 @@
+package com.isrtk.nihtfti;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Avi on 18/06/2014.
+ */
+public class MainService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
